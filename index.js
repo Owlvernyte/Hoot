ec96a925b7ebc70816dcb1f11457345ded55e085
Fiezt <dat09t@gmail.com>
2023-03-15 10:52:56 +0700

    update: run

---
+++ index.js
@@ -0,0 +1,21 @@
+// const { ShardingManager } = require("discord.js");
+// const { token, topggToken, dev } = require("./config.json");
+// const manager = new ShardingManager("./bot.js", { token: token });
+
+// manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
+
+// manager
+// 	.spawn()
+// 	.catch((error) => console.error(`[ERROR/SHARD] Shard failed to spawn.`));
+
+// if (dev !== "on") {
+// 	const { AutoPoster } = require("topgg-autoposter");
+// 	const ap = AutoPoster(topggToken, manager);
+
+// 	ap.on("posted", () => {
+// 		console.log("Posted stats to Top.gg!");
+// 	});
+// }
+
+const bot = require("./bot");
+const server = require("./server");
