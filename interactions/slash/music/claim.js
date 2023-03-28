f22006b49d900ad43d4f3cff66e683571032ea51
Fiezt <dat09t@gmail.com>
2023-03-28 21:27:07 +0700

    update: new claim command

---
+++ interactions/slash/music/claim.js
@@ -0,0 +1,29 @@
+// Deconstructed the constants we need in this file.
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const SuccessEmbed = require("../../../constants/embeds/SuccessEmbed");
+
+module.exports = {
+	// The data needed to register slash commands to Discord.
+
+	data: new SlashCommandBuilder()
+		.setName("claim")
+		.setDescription("Claim the queue in case the owner left"),
+	category: "music",
+	inVoiceChannel: true,
+	queueRequired: true,
+	async execute(interaction) {
+		const { client, guildId, member } = interaction;
+
+		const queue = client.distube.getQueue(guildId);
+
+        if(member.voice.channel.members.has(queue.starter.id))
+            throw new Error(`You have no right to do this!`)
+
+        queue.starter = member;
+
+		interaction.reply({
+			embeds: [new SuccessEmbed(`Claimed!`)],
+		});
+	}
+};
