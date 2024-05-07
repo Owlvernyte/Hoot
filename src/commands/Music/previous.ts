1c573c8bd26939fb8f3ecd2345b46a0f1e8cc10e
Dat Hoang <fiezt@outlook.com>
2024-05-07 21:49:51 +0700

    feat(skip/previous): dynamic voteAction added

--- src/commands/Music/previous.ts
+++ src/commands/Music/previous.ts
@@ -1,7 +1,6 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
-import { HootBaseError } from '../../lib/errors/HootBaseError';
-import { SuccessEmbed } from '../../messages';
+import { voteAction } from '../../lib/distube/voteAction';
 
 @ApplyOptions<Command.Options>({
 	description: 'Play previous song',
@@ -18,51 +17,6 @@ export class UserCommand extends Command {
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		const { guild } = interaction;
-
-		const force = interaction.options.getBoolean('force') || false;
-
-		const queue = this.container.distube.getQueue(guild!.id)!;
-
-		async function previous() {
-			const song = await queue.previous();
-
-			return interaction.reply({
-				embeds: [
-					new SuccessEmbed(`Backed!`).addFields({
-						name: `Now Playing`,
-						value: `[\`${song.name}\`](${song.url})`
-					})
-				]
-			});
-		}
-
-		if (force && interaction.user.id == queue?.owner?.user.id) return previous();
-
-		const membersInVoice = queue.voiceChannel?.members.size || 0;
-
-		const required = Math.ceil(membersInVoice / 2);
-
-		if (queue.backVotes.has(interaction.user.id)) {
-			if (queue?.owner?.user.id == interaction.user.id) {
-				return previous();
-			} else throw new HootBaseError(`You have already voted!`, interaction);
-		}
-
-		const member = await interaction.guild?.members.fetch(interaction.user.id);
-
-		if (!member) {
-			throw new HootBaseError('Member should not be null', interaction);
-		}
-
-		queue.skipVotes.set(member.id, member);
-
-		if (queue.backVotes.size < required) {
-			return interaction.reply({
-				embeds: [new SuccessEmbed(`${queue.backVotes.size}/${required} back votes\nVotes: ${queue.backVotes.map((v) => `${v}`).join(', ')}`)]
-			});
-		}
-
-		return previous();
+		return voteAction(interaction, 'previous');
 	}
 }
--- src/commands/Music/skip.ts
+++ src/commands/Music/skip.ts
@@ -1,8 +1,6 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
-import { HootBaseError } from '../../lib/errors/HootBaseError';
-import { InfoEmbed, SuccessEmbed } from '../../messages';
-
+import { voteAction } from '../../lib/distube/voteAction';
 @ApplyOptions<Command.Options>({
 	description: 'Skip currently playing song',
 	preconditions: ['InVoice', 'InQueue']
@@ -18,51 +16,6 @@ export class UserCommand extends Command {
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		const { guild } = interaction;
-
-		const force = interaction.options.getBoolean('force') || false;
-
-		const queue = this.container.distube.getQueue(guild!.id)!;
-
-		async function skip() {
-			const song = await queue.skip();
-
-			return interaction.reply({
-				embeds: [
-					new SuccessEmbed(`Skipped!`).addFields({
-						name: `Now Playing`,
-						value: `[\`${song.name}\`](${song.url})`
-					})
-				]
-			});
-		}
-
-		if (force && interaction.user.id == queue?.owner?.user.id) return skip();
-
-		const membersInVoice = queue.voiceChannel?.members.size || 0;
-
-		const required = Math.ceil(membersInVoice / 2);
-
-		if (queue.skipVotes.has(interaction.user.id)) {
-			if (queue?.owner?.user.id == interaction.user.id) {
-				return skip();
-			} else throw new HootBaseError(`You have already voted!`, interaction);
-		}
-
-		const member = await interaction.guild?.members.fetch(interaction.user.id);
-
-		if (!member) {
-			throw new HootBaseError('Member should not be null', interaction);
-		}
-
-		queue.skipVotes.set(member.id, member);
-
-		if (queue.skipVotes.size < required) {
-			return interaction.reply({
-				embeds: [new InfoEmbed(`${queue.skipVotes.size}/${required} skip votes\nVotes: ${queue.skipVotes.map((v) => `${v}`).join(', ')}`)]
-			});
-		}
-
-		return skip();
+		return voteAction(interaction, 'skip');
 	}
 }
