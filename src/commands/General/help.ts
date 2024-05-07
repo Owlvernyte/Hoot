0e4f0855eb71e46d494f1c4592b09dc93fa4d926
Dat Hoang <fiezt@outlook.com>
2024-05-07 23:18:44 +0700

    feat(help): add simple command list

--- src/commands/General/help.ts
+++ src/commands/General/help.ts
@@ -1,6 +1,6 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
-import { APIApplicationCommandOptionChoice } from 'discord.js';
+import { APIApplicationCommandOptionChoice, EmbedBuilder } from 'discord.js';
 
 const choices: APIApplicationCommandOptionChoice<string>[] = [
 	{ name: 'Get command list', value: 'commands' },
@@ -28,17 +28,52 @@ export class UserCommand extends Command {
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		// const { guild } = interaction;
-
+		const messagePayload: { content?: string; embeds?: EmbedBuilder[] } = {
+			content: undefined,
+			embeds: undefined
+		};
 		const topic = interaction.options.getString('topic');
 
 		switch (topic) {
-			case 'commands':
+			case 'commands': {
+				const commandStore = this.container.stores.get('commands');
+
+				const categoriedCommands = commandStore.categories.map((category) => ({
+					categoryName: category,
+					commands: commandStore
+						.filter((command) => command.enabled && command.fullCategory.includes(category))
+						.map((command) => ({
+							name: command.name,
+							preconditions: command.preconditions.entries
+						}))
+				}));
+
+				const embed = new EmbedBuilder()
+					.setColor('Random')
+					.setTitle(`COMMAND LIST`)
+					.setThumbnail(this.container.client.user?.displayAvatarURL() || null)
+					.setDescription(
+						`We use \`/command\` (Slash command) only! For further reason click [here](https://support-dev.discord.com/hc/en-us/articles/4404772028055).`
+					)
+					.addFields(
+						...categoriedCommands.map((category) => ({
+							name: category.categoryName,
+							value: category.commands.map((command) => `\`${command.name}\``).join(' • ')
+						}))
+					);
+
+				messagePayload.embeds = [embed];
 				break;
-			case 'support':
+			}
+			case 'support': {
+				const supportGuild = await this.container.client.guilds.fetch(process.env.SUPPORT_SERVER_ID);
+
+				messagePayload.content = `${await supportGuild.invites.create(supportGuild.rulesChannelId!)}`;
+
 				break;
+			}
 		}
 
-		return interaction.reply({ content: 'Hello world!' });
+		return interaction.reply(messagePayload);
 	}
 }
