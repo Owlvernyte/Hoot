a5d9dadf5b8b3f1841048b11bbae0090aff3ee8d
Dat Hoang <fiezt@outlook.com>
2024-05-07 14:47:40 +0700

    feat(precondition): add queueOwner, refactor internal method names

--- src/preconditions/InQueue.ts
+++ src/preconditions/InQueue.ts
@@ -1,21 +1,21 @@
 import { Precondition } from '@sapphire/framework';
 import type { ChatInputCommandInteraction, ContextMenuCommandInteraction, Guild, Message } from 'discord.js';
 
-export class UserPrecondition extends Precondition {
+export class InQueuePrecondition extends Precondition {
 	#message = 'There is nothing playing!';
 	public override messageRun(message: Message) {
-		return this.checkGuildHasQueue(message.guild);
+		return this.doCheck(message.guild);
 	}
 
 	public override chatInputRun(interaction: ChatInputCommandInteraction) {
-		return this.checkGuildHasQueue(interaction.guild);
+		return this.doCheck(interaction.guild);
 	}
 
 	public override contextMenuRun(interaction: ContextMenuCommandInteraction) {
-		return this.checkGuildHasQueue(interaction.guild);
+		return this.doCheck(interaction.guild);
 	}
 
-	private async checkGuildHasQueue(guild: Guild | null) {
+	private async doCheck(guild: Guild | null) {
 		if (!guild) return this.error({ message: this.#message });
 		const queue = await this.container.distube.getQueue(guild.id);
 		return queue ? this.ok() : this.error({ message: this.#message });
