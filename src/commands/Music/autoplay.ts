cee0cbee6ba4ab4680cffea18e9af2077756da31
Dat Hoang <fiezt@outlook.com>
2024-05-07 14:49:30 +0700

    feat(autoplay): command added

--- src/commands/Music/autoplay.ts
+++ src/commands/Music/autoplay.ts
@@ -1,8 +1,10 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
+import { CustomEvents } from '../../lib/constants';
 
 @ApplyOptions<Command.Options>({
-	description: 'A basic slash command'
+	description: 'A basic slash command',
+	preconditions: ['InVoice', 'InQueueWithOwner']
 })
 export class UserCommand extends Command {
 	public override registerApplicationCommands(registry: Command.Registry) {
@@ -14,6 +16,18 @@ export class UserCommand extends Command {
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		return interaction.reply({ content: 'Hello world!' });
+		const { guild } = interaction;
+        
+		if (!guild) throw new Error('Guild should not be null');
+
+		const queue = this.container.distube.getQueue(guild);
+
+		if (!queue) throw new Error('There is nothing playing!');
+
+		const autoplay = queue.toggleAutoplay();
+
+		interaction.reply(`Autoplay is now ${autoplay ? 'enabled' : 'disabled'}!`);
+
+		this.container.client.emit(CustomEvents.UpdatePanel, interaction);
 	}
 }
