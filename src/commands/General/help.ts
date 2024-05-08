67facc59389f3ef84c880e33c3e6d59fb5fd024d
Dat Hoang <fiezt@outlook.com>
2024-05-08 14:49:08 +0700

    feat(help): cleanup codes

--- src/commands/General/help.ts
+++ src/commands/General/help.ts
@@ -1,10 +1,11 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
-import { APIApplicationCommandOptionChoice, EmbedBuilder } from 'discord.js';
+import { APIApplicationCommandOptionChoice, ApplicationCommandType, EmbedBuilder, chatInputApplicationCommandMention } from 'discord.js';
+
+const dev = process.env.NODE_ENV !== 'production';
 
 const choices: APIApplicationCommandOptionChoice<string>[] = [
 	{ name: 'Get command list', value: 'commands' },
-	// { name: 'Get relevant links', value: 'links' },
 	{ name: 'Support server', value: 'support' }
 ];
 
@@ -28,52 +29,60 @@ export class UserCommand extends Command {
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		const messagePayload: { content?: string; embeds?: EmbedBuilder[] } = {
-			content: undefined,
-			embeds: undefined
-		};
 		const topic = interaction.options.getString('topic');
 
-		switch (topic) {
-			case 'commands': {
-				const commandStore = this.container.stores.get('commands');
+        if (topic === 'commands') {
+            return this.commandsOptionRun(interaction);
+        }
 
-				const categoriedCommands = commandStore.categories.map((category) => ({
-					categoryName: category,
-					commands: commandStore
-						.filter((command) => command.enabled && command.fullCategory.includes(category))
-						.map((command) => ({
-							name: command.name,
-							preconditions: command.preconditions.entries
-						}))
-				}));
+        return this.supportOptionRun(interaction);
+	}
 
-				const embed = new EmbedBuilder()
-					.setColor('Random')
-					.setTitle(`COMMAND LIST`)
-					.setThumbnail(this.container.client.user?.displayAvatarURL() || null)
-					.setDescription(
-						`We use \`/command\` (Slash command) only! For further reason click [here](https://support-dev.discord.com/hc/en-us/articles/4404772028055).`
-					)
-					.addFields(
-						...categoriedCommands.map((category) => ({
-							name: category.categoryName,
-							value: category.commands.map((command) => `\`${command.name}\``).join(' • ')
-						}))
-					);
+	private async commandsOptionRun(interaction: Command.ChatInputCommandInteraction) {
+		const { guild } = interaction;
+		const applicationCommands = (dev ? await guild!.commands.fetch() : await this.container.client.application!.commands.fetch())
+			.filter((c) => c.type === ApplicationCommandType.ChatInput)
+			.map(({ id, name, description }) => ({
+				id,
+				name,
+				description
+			}));
 
-				messagePayload.embeds = [embed];
-				break;
-			}
-			case 'support': {
-				const supportGuild = await this.container.client.guilds.fetch(process.env.SUPPORT_SERVER_ID);
+		const commandStore = this.container.stores.get('commands');
 
-				messagePayload.content = `${await supportGuild.invites.create(supportGuild.rulesChannelId!)}`;
+		const categorizedCommands = commandStore.categories.map((category) => ({
+			categoryName: category,
+			commands: commandStore
+				.filter((command) => command.enabled && command.fullCategory.includes(category))
+				.map((command) => applicationCommands.find((ac) => ac.name === command.name))
+		}));
 
-				break;
-			}
-		}
+		const embed = new EmbedBuilder()
+			.setColor('Random')
+			.setTitle(`COMMAND LIST`)
+			.setThumbnail(this.container.client.user?.displayAvatarURL() || null)
+			.setDescription(
+				`We use \`/command\` (Slash command) only! For further reason click [here](https://support-dev.discord.com/hc/en-us/articles/4404772028055).`
+			)
+			.addFields(
+				...categorizedCommands.map((category) => ({
+					name: category.categoryName,
+					value: category.commands
+						.map((command) => chatInputApplicationCommandMention(command?.name || 'unknownName', command?.id || 'unknownId'))
+						.join(' • ')
+				}))
+			);
 
-		return interaction.reply(messagePayload);
+		return interaction.reply({
+			embeds: [embed]
+		});
+	}
+
+	private async supportOptionRun(interaction: Command.ChatInputCommandInteraction) {
+		const supportGuild = await this.container.client.guilds.fetch(process.env.SUPPORT_SERVER_ID);
+
+		return interaction.reply({
+			content: `${await supportGuild.invites.create(supportGuild.rulesChannelId!)}`
+		});
 	}
 }
