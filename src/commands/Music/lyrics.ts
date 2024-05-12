00b9b613ebbdb798132f368e3dad5590b4c23f63
Dat Hoang <fiezt@outlook.com>
2024-05-12 13:44:50 +0700

    feat(lyrics): preprocess song name

--- src/commands/Music/lyrics.ts
+++ src/commands/Music/lyrics.ts
@@ -1,10 +1,11 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
+import { HootBaseError } from '../../lib/errors/HootBaseError';
 
 @ApplyOptions<Command.Options>({
 	description: 'Search lyrics of a song',
-	enabled: false,
-    runIn: ['GUILD_ANY']
+	preconditions: ['GuildOnly'],
+	runIn: ['GUILD_ANY']
 })
 export class UserCommand extends Command {
 	public override registerApplicationCommands(registry: Command.Registry) {
@@ -13,13 +14,28 @@ export class UserCommand extends Command {
 				.setName(this.name)
 				.setDescription(this.description)
 				.addStringOption((option) => option.setName('song').setDescription('Song name/related string to search'))
-				.addBooleanOption((option) =>
-					option.setName('ephemeral').setDescription('Whether to show the lyrics ephemeral or not | Default: false')
-				)
 		);
 	}
 
 	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const { guild } = interaction;
+		let song = interaction.options.getString('song');
+		const queue = this.container.distube.getQueue(guild!.id);
+
+		if (!song) {
+			if (!queue || !queue.songs.length) {
+				throw new HootBaseError('There is nothing playing!', interaction);
+			}
+
+			const np = queue.songs[0];
+			song = np.name?.toLowerCase() || np.url;
+		}
+
+		song = song.replace(
+			/lyrics|lyric|lyrical|official music video|\(official music video\)|audio|official|official video|official video hd|official hd video|offical video music|\(offical video music\)|extended|hd|(\[.+\])/gi,
+			''
+		);
+
 		return interaction.reply({ content: 'Hello world!' });
 	}
 }
